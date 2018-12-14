8ce88e656c1de853089e4a0c83fe80df2a20e225
Jafar Qaddoumi <jafarkhq@gmail.com>
2018-12-14 14:12:22 +0100

    add account service

--- src/main/java/me/memleak/revolutfers/service/AccountService.java
+++ src/main/java/me/memleak/revolutfers/service/AccountService.java
@@ -1,20 +1,39 @@
 package me.memleak.revolutfers.service;
 
+import me.memleak.revolutfers.exception.AccountNotFoundException;
 import me.memleak.revolutfers.model.Account;
+import me.memleak.revolutfers.repository.AccountMapRepository;
 
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.List;
+import java.util.function.DoubleFunction;
 
+@Singleton
 public class AccountService {
 
+  private final AccountMapRepository repository;
+
+  @Inject
+  public AccountService(AccountMapRepository repository) {
+    this.repository = repository;
+  }
+
   public List<Account> getAll() {
-    return null;
+    return repository.findAll();
   }
 
   public Account get(long id) {
-    return null;
+    return repository.find(id)
+        .orElseThrow(() -> new AccountNotFoundException("Cant find Account with id: {}", id));
   }
 
-  public Account create(double amount) {
-    return null;
+  public Account create(double balance) {
+    return repository.create(toBankingBalance.apply(balance));
   }
+
+  private DoubleFunction<BigDecimal> toBankingBalance =
+      (d) -> BigDecimal.valueOf(d).setScale(2, RoundingMode.CEILING);
 }

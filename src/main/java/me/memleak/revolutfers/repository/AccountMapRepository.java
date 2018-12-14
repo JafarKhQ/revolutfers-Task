ea2e888032b044bbe5cbbbb01fde9b113f9c59d9
Jafar Qaddoumi <jafarkhq@gmail.com>
2018-12-14 13:23:21 +0100

    add account repo

---
+++ src/main/java/me/memleak/revolutfers/repository/AccountMapRepository.java
@@ -0,0 +1,28 @@
+package me.memleak.revolutfers.repository;
+
+import me.memleak.revolutfers.model.Account;
+
+import javax.inject.Singleton;
+import java.math.BigDecimal;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicLong;
+
+@Singleton
+public class AccountMapRepository {
+
+  private final AtomicLong idGenerator = new AtomicLong();
+  private final Map<Long, Account> accounts = new HashMap<>();
+
+  public Account create(BigDecimal balance) {
+    long id = idGenerator.getAndIncrement();
+    return accounts.put(id, new Account(id, balance));
+  }
+
+  public List<Account> findAll() {
+    return new ArrayList<>(accounts.values());
+  }
+
+  public Optional<Account> find(long id) {
+    return Optional.ofNullable(accounts.get(id));
+  }
+}
